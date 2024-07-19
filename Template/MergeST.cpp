b0858b044325911aedf2ede268c8edc317e2eff0
Kien Pham <124032637+kaydenpham27@users.noreply.github.com>
2024-07-20 02:01:40 +1000

    Update MergeST.cpp

--- Template/MergeST.cpp
+++ Template/MergeST.cpp
@@ -4,7 +4,7 @@ struct MergeTree {
   vector<int> Combine(vector<int> lf, vector<int> rg) {
     vector<int> res;
     merge(all(lf), all(rg), back_inserter(res));
-    res.erase(unique(all(res)), res.end());
+    res.erase(unique(all(res)), res.end());    
     return res;
   }
   void Build(int id, int l, int r) {
@@ -15,7 +15,7 @@ struct MergeTree {
     int mid = (l + r)/2;
     Build(id * 2, l, mid);
     Build(id * 2 + 1, mid + 1, r);
-    merge(all(st[id * 2]), all(st[id * 2 + 1]), back_inserter(st[id]));
+    st[id] = Combine(st[id * 2], st[id * 2 + 1]);
   }
   vector<int> Get(int id, int l, int r, int u, int v) {
     if (v < l || u > r) return {};
