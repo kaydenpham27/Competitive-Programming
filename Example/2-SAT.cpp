1d3780fc296fe5e32355d551c9625435c7db755b
Kien Pham <124032637+kaydenpham27@users.noreply.github.com>
2024-08-13 17:36:43 +1000

    Create 2-SAT.cpp

---
+++ Example/2-SAT.cpp
@@ -0,0 +1,170 @@
+/*
+  Problem: https://codeforces.com/problemset/problem/1971/H
+  Category: 2-SAT (Graphs)
+  Template: https://github.com/atcoder/ac-library/blob/master/atcoder/twosat.hpp
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define i64 long long
+#define mp make_pair
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+namespace atcoder {
+namespace internal {
+ 
+template <class E> struct csr {
+    std::vector<int> start;
+    std::vector<E> elist;
+    csr(int n, const std::vector<std::pair<int, E>>& edges)
+        : start(n + 1), elist(edges.size()) {
+        for (auto e : edges) {
+            start[e.first + 1]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            start[i] += start[i - 1];
+        }
+        auto counter = start;
+        for (auto e : edges) {
+            elist[counter[e.first]++] = e.second;
+        }
+    }
+};
+ 
+// Reference:
+// R. Tarjan,
+// Depth-First Search and Linear Graph Algorithms
+struct scc_graph {
+  public:
+    scc_graph(int n) : _n(n) {}
+ 
+    int num_vertices() { return _n; }
+ 
+    void add_edge(int from, int to) { edges.push_back({from, {to}}); }
+ 
+    // @return pair of (# of scc, scc id)
+    std::pair<int, std::vector<int>> scc_ids() {
+        auto g = csr<edge>(_n, edges);
+        int now_ord = 0, group_num = 0;
+        std::vector<int> visited, low(_n), ord(_n, -1), ids(_n);
+        visited.reserve(_n);
+        auto dfs = [&](auto self, int v) -> void {
+            low[v] = ord[v] = now_ord++;
+            visited.push_back(v);
+            for (int i = g.start[v]; i < g.start[v + 1]; i++) {
+                auto to = g.elist[i].to;
+                if (ord[to] == -1) {
+                    self(self, to);
+                    low[v] = std::min(low[v], low[to]);
+                } else {
+                    low[v] = std::min(low[v], ord[to]);
+                }
+            }
+            if (low[v] == ord[v]) {
+                while (true) {
+                    int u = visited.back();
+                    visited.pop_back();
+                    ord[u] = _n;
+                    ids[u] = group_num;
+                    if (u == v) break;
+                }
+                group_num++;
+            }
+        };
+        for (int i = 0; i < _n; i++) {
+            if (ord[i] == -1) dfs(dfs, i);
+        }
+        for (auto& x : ids) {
+            x = group_num - 1 - x;
+        }
+        return {group_num, ids};
+    }
+ 
+    std::vector<std::vector<int>> scc() {
+        auto ids = scc_ids();
+        int group_num = ids.first;
+        std::vector<int> counts(group_num);
+        for (auto x : ids.second) counts[x]++;
+        std::vector<std::vector<int>> groups(ids.first);
+        for (int i = 0; i < group_num; i++) {
+            groups[i].reserve(counts[i]);
+        }
+        for (int i = 0; i < _n; i++) {
+            groups[ids.second[i]].push_back(i);
+        }
+        return groups;
+    }
+ 
+  private:
+    int _n;
+    struct edge {
+        int to;
+    };
+    std::vector<std::pair<int, edge>> edges;
+};
+ 
+}  // namespace internal
+ 
+}  // namespace atcoder
+ 
+#include <cassert>
+#include <vector>
+ 
+namespace atcoder {
+ 
+// Reference:
+// B. Aspvall, M. Plass, and R. Tarjan,
+// A Linear-Time Algorithm for Testing the Truth of Certain Quantified Boolean
+// Formulas
+struct two_sat {
+  public:
+    two_sat() : _n(0), scc(0) {}
+    two_sat(int n) : _n(n), _answer(n), scc(2 * n) {}
+ 
+    void add_clause(int i, bool f, int j, bool g) {
+        assert(0 <= i && i < _n);
+        assert(0 <= j && j < _n);
+        scc.add_edge(2 * i + (f ? 0 : 1), 2 * j + (g ? 1 : 0));
+        scc.add_edge(2 * j + (g ? 0 : 1), 2 * i + (f ? 1 : 0));
+    }
+    bool satisfiable() {
+        auto id = scc.scc_ids().second;
+        for (int i = 0; i < _n; i++) {
+            if (id[2 * i] == id[2 * i + 1]) return false;
+            _answer[i] = id[2 * i] < id[2 * i + 1];
+        }
+        return true;
+    }
+    std::vector<bool> answer() { return _answer; }
+ 
+  private:
+    int _n;
+    std::vector<bool> _answer;
+    internal::scc_graph scc;
+};
+ 
+}  // namespace atcoder
+void Solve(void) {
+  int N; cin >> N;
+  vector<vector<int>> g(3, vector<int> (N));
+  for (int i = 0; i < 3; i ++) {
+    for (int j = 0; j < N; j ++) {
+      cin >> g[i][j];
+    }
+  }
+  atcoder::two_sat ts(N);
+  for (int j = 0; j < N; j ++) {
+    for (int i = 0; i < 3; i ++) {
+      int nxt = (i + 1) % 3;
+      ts.add_clause(abs(g[i][j]) - 1, (g[i][j] > 0), abs(g[nxt][j]) - 1, (g[nxt][j] > 0));
+    }  
+  }
+  cout << (ts.satisfiable() ? "YES" : "NO") << "\n";
+}
+signed main() {
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  cout << fixed << setprecision(10);
+  int Tests = 1; cin >> Tests;
+  while (Tests --) {
+    Solve();
+  }
+}
